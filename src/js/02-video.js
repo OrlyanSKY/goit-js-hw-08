bce3224fcca7291fd39bb008a99569061f3c94bc
Denis Orlyansky <000slim001@gmail.com>
2022-08-07 16:50:48 +0300

    complete second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+const playbackTime = localStorage.getItem('videoplayer-current-time');
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+if (!playbackTime) {
+  player.setCurrentTime(0);
+} else {
+  player.setCurrentTime(playbackTime);
+}
+
+function currentTime(evt) {
+  const currentPlaybackTime = evt.seconds;
+  localStorage.setItem('videoplayer-current-time', currentPlaybackTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash.throttle';
 
+const formInput = document.querySelector('.feedback-form');
+
+formInput.addEventListener('input', onFormData);
+formInput.addEventListener('submit', onFormSubmit);
+
+const formData = {};
+
+function onFormData(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+
+  const currentValue = localStorage.getItem('feedback-form-state');
+  const parseValue = JSON.parse(currentValue);
+  console.log(parseValue);
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+}
